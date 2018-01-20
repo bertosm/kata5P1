b909f4df271fd314742133affcd5d4eb649e9065
Usuario <Usuario@DESKTOP-8L7RG71>
2018-01-20 05:01:54 +0000

    kata5 parte1 /lectura desde fichero

--- src/kata5p1/Kata5P1.java
+++ src/kata5p1/Kata5P1.java
@@ -1,6 +1,11 @@
 package kata5p1;
 
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -9,7 +14,7 @@ import java.sql.Statement;
 
 public class Kata5P1 {
 
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException, FileNotFoundException, IOException {
         
         Class.forName("org.sqlite.JDBC");
         Connection connec = DriverManager.getConnection("jdbc:sqlite:kata5.db");
@@ -21,6 +26,18 @@ public class Kata5P1 {
         }
         
         st.execute("CREATE TABLE IF NOT EXISTS MAIL ('id' INTEGER PRIMARY KEY AUTOINCREMENT , 'Mail' TEXT NOT NULL);");
+        
+        String filename = "emailsFile.txt";
+        
+        BufferedReader reader = new BufferedReader(new FileReader(new File(filename)));
+        String mail;
+        String query2;
+        
+        while ((mail = reader.readLine()) != null) {
+            if (!mail.contains("@")) continue;
+            query2 = "ISERT INTO MAIL (mail) VALUES ('" + mail + "');";
+            st.executeUpdate(query2);
+        }
     }
     
 }
